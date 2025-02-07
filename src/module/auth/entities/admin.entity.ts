c01432e7bfe040dc981b23f4af1459f7f89f8480
Amir-Fayyaz <fayamir54@gmail.com>
2025-02-07 05:03:48 -0800

    update adminEntity

--- src/module/auth/entities/admin.entity.ts
+++ src/module/auth/entities/admin.entity.ts
@@ -3,19 +3,27 @@ import {
   CreateDateColumn,
   Entity,
   PrimaryGeneratedColumn,
+  Unique,
   UpdateDateColumn,
 } from 'typeorm';
 
 @Entity('Admins')
+@Unique(['email', 'username'])
 export class AdminEntity {
   @PrimaryGeneratedColumn({ type: 'bigint' })
   id: number;
 
-  @Column({ type: 'varchar', nullable: false, unique: true })
+  @Column({ type: 'varchar' })
+  username: string;
+
+  @Column({ type: 'varchar', nullable: false })
   email: string;
 
   @Column({ type: 'varchar', nullable: false })
   password: string;
+
+  @Column({ default: false })
+  isSuperAdmin: Boolean;
 
   @CreateDateColumn()
   createdAt: Date;
