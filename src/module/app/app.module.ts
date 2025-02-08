aa747818c44bbe43120da7e65d2a52c417a89371
Amir-Fayyaz <fayamir54@gmail.com>
2025-02-08 05:58:10 -0800

    add AuthModule to project

--- src/module/app/app.module.ts
+++ src/module/app/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmConfig } from 'src/common/config/TypeOrm.config';
 import { UserModule } from '../users/user.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(TypeOrmConfig), UserModule],
+  imports: [TypeOrmModule.forRoot(TypeOrmConfig), UserModule, AuthModule],
   controllers: [],
   providers: [],
 })
