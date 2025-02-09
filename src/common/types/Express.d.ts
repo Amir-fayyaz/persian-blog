bb27a43359d0cb40813a1d773b88370534bb455e
Amir-Fayyaz <fayamir54@gmail.com>
2025-02-09 09:35:38 -0800

    add admin to Request type

--- src/common/types/Express.d.ts
+++ src/common/types/Express.d.ts
@@ -1,9 +1,11 @@
+import { AdminEntity } from 'src/module/auth/entities/admin.entity';
 import { UserEntity } from 'src/module/users/entities/user.entity';
 
 declare global {
   namespace Express {
     interface Request {
       user?: UserEntity;
+      admin?: AdminEntity;
     }
   }
 }
