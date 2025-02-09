d52e636cd7e4efef578b743ad4932d7caad243b7
Amir-Fayyaz <fayamir54@gmail.com>
2025-02-09 09:57:17 -0800

    add guard to userClient

--- src/module/users/client/user.client.controller.ts
+++ src/module/users/client/user.client.controller.ts
@@ -1,19 +1,30 @@
-import { Body, Controller, Delete, Get, Param, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Put,
+  UseGuards,
+} from '@nestjs/common';
 import { UserClientService } from './user.client.service';
 import { User } from 'src/common/decorators/getUser.decorator';
 import { UserEntity } from '../entities/user.entity';
 import { EditUserClientDto } from './dto/editUser.client.dto';
+import { UserGuard } from 'src/module/auth/guards/user.guard';
 
-@Controller('users')
+@Controller('users/client')
 export class UserClientController {
   constructor(private readonly UserClientService: UserClientService) {}
 
   @Get('profile')
+  @UseGuards(UserGuard)
   async getProfile(@User() user: UserEntity) {
     return user;
   }
 
   @Put('profile/:id')
+  @UseGuards(UserGuard)
   async updateProfile(
     @Param('id') id: number,
     @User() user: UserEntity,
@@ -23,6 +34,7 @@ export class UserClientController {
   }
 
   @Delete('profile')
+  @UseGuards(UserGuard)
   async deleteAcount(@User() user: UserEntity) {
     return await this.UserClientService.deleteAcount(user);
   }
--- src/module/users/client/user.elient.module.ts
+++ src/module/users/client/user.elient.module.ts
@@ -3,10 +3,18 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from '../entities/user.entity';
 import { UserClientService } from './user.client.service';
 import { UserClientController } from './user.client.controller';
+import { JwtService } from '@nestjs/jwt';
+import { UserAdminFactory } from '../admin/user.admin.factory';
+import { UserAdminService } from '../admin/user.admin.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserEntity])],
   controllers: [UserClientController],
-  providers: [UserClientService],
+  providers: [
+    UserClientService,
+    JwtService,
+    UserAdminFactory,
+    UserAdminService,
+  ],
 })
 export class UserClientModule {}
