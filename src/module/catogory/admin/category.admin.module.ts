30cfe4d071af7533496ff2858446ab1ab57a06eb
Amir-Fayyaz <fayamir54@gmail.com>
2025-02-12 09:24:02 -0800

    add guard dependency

--- src/module/catogory/admin/category.admin.module.ts
+++ src/module/catogory/admin/category.admin.module.ts
@@ -6,10 +6,22 @@ import { CategoryAdminController } from './controllers/category.admin.controller
 import { SubCategoryAdminController } from './controllers/subCategory.admin.controller';
 import { CategoryAdminService } from './services/category.admin.service';
 import { SubCategoryAdminService } from './services/subCategory.admin.service';
+import { AuthAdminFactory } from 'src/module/auth/admin/auth.admin.factory';
+import { JwtService } from '@nestjs/jwt';
+import { AuthAdminService } from 'src/module/auth/admin/auth.admin.service';
+import { AdminEntity } from 'src/module/auth/entities/admin.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CategoryEntity, subCategoryEntity])],
+  imports: [
+    TypeOrmModule.forFeature([CategoryEntity, subCategoryEntity, AdminEntity]),
+  ],
   controllers: [CategoryAdminController, SubCategoryAdminController],
-  providers: [CategoryAdminService, SubCategoryAdminService],
+  providers: [
+    CategoryAdminService,
+    SubCategoryAdminService,
+    AuthAdminFactory,
+    AuthAdminService,
+    JwtService,
+  ],
 })
 export class CategoryAdminModule {}
