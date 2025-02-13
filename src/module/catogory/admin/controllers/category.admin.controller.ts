3b18dc29ec752093d82bbe59b14899619c61ffe2
Amir-Fayyaz <fayamir54@gmail.com>
2025-02-13 03:50:36 -0800

    add swagger for getCategory api

--- src/module/catogory/admin/controllers/category.admin.controller.ts
+++ src/module/catogory/admin/controllers/category.admin.controller.ts
@@ -14,7 +14,7 @@ import {
 import { CategoryAdminService } from '../services/category.admin.service';
 import { CreateCategoryDto } from '../dto/category/createCategory.admin.dto';
 import { AdminGuard } from 'src/module/auth/guards/admin.guard';
-import { ApiBody, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { ApiBody, ApiOperation, ApiQuery, ApiResponse } from '@nestjs/swagger';
 import { CategoryEntity } from '../../entities/category.entity';
 
 @Controller('category/admin')
@@ -60,6 +60,52 @@ export class CategoryAdminController {
 
   //GET -
   @Get()
+  @ApiOperation({
+    summary: 'Get a paginated list of categories',
+    description:
+      'Fetch a list of categories with pagination support. You can specify the page number with the "page" query parameter.',
+  })
+  @ApiQuery({
+    name: 'page',
+    description: 'the page number for pagination , default is 1',
+    type: Number,
+    example: 1,
+    required: false,
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Successfully fetched categories list',
+    schema: {
+      type: 'object',
+      properties: {
+        totalPages: {
+          type: 'number',
+          description:
+            'The total number of pages available based on the pagination.',
+        },
+        categories: {
+          type: 'array',
+          items: {
+            type: 'object',
+            properties: {
+              id: { type: 'number', description: 'Category ID' },
+              title: { type: 'string', description: 'Category Title' },
+              createdAt: {
+                type: 'string',
+                description: 'Creation timestamp of the category',
+                format: 'date-time',
+              },
+            },
+          },
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Unauthorized - JWT token is missing or invalid.',
+  })
+  @HttpCode(HttpStatus.OK)
   async getCategories(
     @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
   ) {
