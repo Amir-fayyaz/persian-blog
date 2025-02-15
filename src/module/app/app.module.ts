848ad1103cf581e836820c794e1e49302a994470
Amir-Fayyaz <fayamir54@gmail.com>
2025-02-15 09:19:51 -0800

    add throttle config to AppModule

--- src/module/app/app.module.ts
+++ src/module/app/app.module.ts
@@ -4,9 +4,11 @@ import { TypeOrmConfig } from 'src/common/config/TypeOrm.config';
 import { UserModule } from '../users/user.module';
 import { AuthModule } from '../auth/auth.module';
 import { CategoryModule } from '../catogory/category.module';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
+    ThrottlerModule.forRoot({ ttl: 20, limit: 4 }),
     TypeOrmModule.forRoot(TypeOrmConfig),
     UserModule,
     AuthModule,
