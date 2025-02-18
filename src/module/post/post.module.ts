221335b184cf8e1bd455359571a6c9043859401d
Amir-Fayyaz <fayamir54@gmail.com>
2025-02-18 14:56:24 -0800

    add PostAdminModule to app

--- src/module/post/post.module.ts
+++ src/module/post/post.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { PostAdminModule } from './admin/post.admin.module';
 
 @Module({
-  imports: [],
+  imports: [PostAdminModule],
   controllers: [],
   providers: [],
 })
