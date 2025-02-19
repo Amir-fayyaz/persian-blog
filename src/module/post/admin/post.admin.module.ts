9ad0392eb82a298ec38b0ac51273a5e2c3652900
Amir-Fayyaz <fayamir54@gmail.com>
2025-02-18 23:55:37 -0800

    add dependencies for AdminGuard

--- src/module/post/admin/post.admin.module.ts
+++ src/module/post/admin/post.admin.module.ts
@@ -3,10 +3,14 @@ import { PostAdminController } from './controllers/post.admin.controller';
 import { PostAdminService } from './services/post.admin.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostEntity } from '../entities/post.entity';
+import { AuthAdminFactory } from 'src/module/auth/admin/auth.admin.factory';
+import { AuthAdminService } from 'src/module/auth/admin/auth.admin.service';
+import { AdminEntity } from 'src/module/auth/entities/admin.entity';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PostEntity])],
+  imports: [TypeOrmModule.forFeature([PostEntity, AdminEntity])],
   controllers: [PostAdminController],
-  providers: [PostAdminService],
+  providers: [PostAdminService, AuthAdminFactory, AuthAdminService, JwtService],
 })
 export class PostAdminModule {}
