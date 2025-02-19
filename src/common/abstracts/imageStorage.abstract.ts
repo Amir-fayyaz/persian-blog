c5219b4050edee089e1c2d97a90c3df0f1655707
Amir-Fayyaz <fayamir54@gmail.com>
2025-02-19 08:59:15 -0800

    update ImageStorage.abstract.ts

--- src/common/abstracts/imageStorage.abstract.ts
+++ src/common/abstracts/imageStorage.abstract.ts
@@ -1,4 +1,7 @@
 export abstract class StorageService {
   abstract uploadFile(file: Express.Multer.File, path: string);
-  abstract deleteFile(name: string, path: string): Promise<void>;
+  abstract deleteFile(
+    name: string,
+    path: { path: string; prefix?: string },
+  ): Promise<void>;
 }
