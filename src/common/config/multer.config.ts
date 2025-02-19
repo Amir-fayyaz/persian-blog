0e743750dae5b0fac5760cbfa23ec0fa78142496
Amir-Fayyaz <fayamir54@gmail.com>
2025-02-19 11:09:52 -0800

    update multerConfig for dynamic destination

--- src/common/config/multer.config.ts
+++ src/common/config/multer.config.ts
@@ -2,10 +2,29 @@ import { BadRequestException } from '@nestjs/common';
 import { MulterOptions } from '@nestjs/platform-express/multer/interfaces/multer-options.interface';
 import { Request } from 'express';
 import { diskStorage } from 'multer';
+import * as fs from 'fs';
 
 export const MulterOption: MulterOptions = {
   storage: diskStorage({
-    destination: 'static/uploads',
+    destination: (req: Request, file: Express.Multer.File, cb: Function) => {
+      const uploadType = req.query.uploadType as string;
+      let path = '';
+
+      if (uploadType === 'profile') {
+        path = './uploads/profiles';
+      } else if (uploadType === 'post') {
+        path = './uploads/posts';
+      } else {
+        // پیش‌فرض یا خطا
+        return cb(new Error('Invalid upload type!'), '');
+      }
+
+      if (!fs.existsSync(path)) {
+        fs.mkdirSync(path, { recursive: true });
+      }
+
+      cb(null, path);
+    },
 
     filename: (req: Request, file: Express.Multer.File, cb: Function) => {
       const uniqueName: number = Date.now() + -Math.round(Math.random() * 1e9);
