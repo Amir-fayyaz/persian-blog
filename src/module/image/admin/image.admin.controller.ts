a340f784d0b45ebbfeac10e6272d33911f11af26
Amir-Fayyaz <fayamir54@gmail.com>
2025-02-20 12:21:18 -0800

    add swagger to uploadFile api

--- src/module/image/admin/image.admin.controller.ts
+++ src/module/image/admin/image.admin.controller.ts
@@ -1,10 +1,8 @@
 import {
   Controller,
-  DefaultValuePipe,
   HttpCode,
   HttpStatus,
   Post,
-  Query,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
@@ -12,12 +10,40 @@ import { ImageAdminService } from './image.admin.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { MulterOption } from 'src/common/config/multer.config';
 import { UploadFileType } from '../enums/uploadType.enum';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiQuery,
+  ApiResponse,
+} from '@nestjs/swagger';
 
+@ApiBearerAuth()
 @Controller('api/v1/admin/image')
 export class ImageAdminController {
   constructor(private readonly ImageService: ImageAdminService) {}
 
   @Post()
+  @ApiOperation({
+    summary: 'for upload profile/post image',
+    description: 'send a valid image file & return file info',
+  })
+  @ApiQuery({
+    name: 'uploadType',
+    description: 'is this image for post or profile',
+    enum: UploadFileType,
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'file uploaded succcessfully & recive file info',
+  })
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: 'Unauthorized',
+  })
+  @ApiResponse({
+    status: HttpStatus.BAD_REQUEST,
+    description: 'Validation error , Invalid uploadType or file',
+  })
   @HttpCode(HttpStatus.OK)
   @UseInterceptors(FileInterceptor('image', MulterOption))
   async uploadFile(@UploadedFile() file: Express.Multer.File) {
