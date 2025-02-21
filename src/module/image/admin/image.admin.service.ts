805e35e76647bcd6b8ac8c8d6693b5477fec6bd7
Amir-Fayyaz <fayamir54@gmail.com>
2025-02-21 02:46:22 -0800

    add readFile service to app

--- src/module/image/admin/image.admin.service.ts
+++ src/module/image/admin/image.admin.service.ts
@@ -1,4 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+import { promises as fs } from 'fs';
+import * as mime from 'mime-types';
 
 @Injectable()
 export class ImageAdminService {
@@ -6,5 +9,17 @@ export class ImageAdminService {
 
   public async uploadImage(file: Express.Multer.File) {
     return file;
+  }
+
+  public async readFile(path: string) {
+    const Buffer = await fs.readFile(path);
+
+    if (!Buffer) {
+      throw new NotFoundException('There is no file with this path');
+    }
+
+    const mimeType = mime.lookup(path) || 'application/octet-stream';
+
+    return { Buffer, mimeType };
   }
 }
