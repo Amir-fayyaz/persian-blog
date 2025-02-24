8c777b5062a18bf99ceb668596f109c4f996acfa
Amir-Fayyaz <fayamir54@gmail.com>
2025-02-24 14:04:08 -0800

    add swagger to getReports api

--- src/module/post/admin/controllers/post-report.admin.controller.ts
+++ src/module/post/admin/controllers/post-report.admin.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
 import { PostReportService } from '../services/post-report.admin.service';
+import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
 
+@ApiBearerAuth()
 @Controller('api/v1/admin/post-reports')
 export class PostReportController {
   constructor(private readonly PostReportService: PostReportService) {}
 
   @Get()
   @HttpCode(HttpStatus.OK)
+  @ApiOperation({
+    summary: 'get all post-reports',
+  })
   async getReports() {
     return await this.PostReportService.findAllReports();
   }
