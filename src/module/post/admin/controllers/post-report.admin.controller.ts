2e1a18fbf4ca3d4635f3189213b777a25e76dfc4
Amir-Fayyaz <fayamir54@gmail.com>
2025-02-24 13:59:33 -0800

    add getAllReports api to app

--- src/module/post/admin/controllers/post-report.admin.controller.ts
+++ src/module/post/admin/controllers/post-report.admin.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
+import { PostReportService } from '../services/post-report.admin.service';
 
-@Controller('api/v1/admin/post-report')
-export class PostReportController {}
+@Controller('api/v1/admin/post-reports')
+export class PostReportController {
+  constructor(private readonly PostReportService: PostReportService) {}
+
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  async getReports() {
+    return await this.PostReportService.findAllReports();
+  }
+}
