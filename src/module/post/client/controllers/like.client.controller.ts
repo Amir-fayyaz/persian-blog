c743f58c46af9964a573099f4f085ac6c1d7c1d6
Amir-Fayyaz <fayamir54@gmail.com>
2025-02-25 15:19:12 -0800

    add swagger to getPost-like api

--- src/module/post/client/controllers/like.client.controller.ts
+++ src/module/post/client/controllers/like.client.controller.ts
@@ -7,12 +7,20 @@ import {
   ParseIntPipe,
 } from '@nestjs/common';
 import { LikeClientService } from '../services/like.client.service';
+import { ApiOperation, ApiParam } from '@nestjs/swagger';
 
 @Controller('api/v1/client/likes')
 export class LikeClientController {
   constructor(private readonly LikeService: LikeClientService) {}
 
   @Get(':id')
+  @ApiOperation({
+    summary: 'for get all likes of special post',
+  })
+  @ApiParam({
+    name: 'id',
+    description: 'PostId of you want to recive that likes',
+  })
   @HttpCode(HttpStatus.OK)
   async getLikeForPost(@Param('id', ParseIntPipe) postId: number) {
     return await this.LikeService.getLikesForPost(postId);
