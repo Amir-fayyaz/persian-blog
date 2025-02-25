9d31b5dcd54e358687a9b20435e4a871d199d198
Amir-Fayyaz <fayamir54@gmail.com>
2025-02-25 05:11:34 -0800

    add swagger tp getPosts api in post.client.controller.ts

--- src/module/post/client/controllers/post.client.controller.ts
+++ src/module/post/client/controllers/post.client.controller.ts
@@ -9,12 +9,30 @@ import {
 } from '@nestjs/common';
 import { PostClientService } from '../services/post.client.service';
 import { PostSorting } from '../../enums/Post.sorting.enum';
+import { ApiOperation, ApiParam, ApiQuery } from '@nestjs/swagger';
 
 @Controller('api/v1/client/posts')
 export class PostClientController {
   constructor(private readonly PostClientService: PostClientService) {}
 
+  //GET -
   @Get()
+  @ApiOperation({
+    summary: 'for recive posts with pagination',
+  })
+  @ApiQuery({
+    name: 'page',
+    default: 1,
+    description: 'required field for pagination',
+    required: false,
+  })
+  @ApiQuery({
+    name: 'sortingBy',
+    description: 'its for sorting by newest posts or popular posts',
+    enum: PostSorting,
+    required: false,
+    default: PostSorting.NEW,
+  })
   @HttpCode(HttpStatus.OK)
   async getPosts(
     @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
