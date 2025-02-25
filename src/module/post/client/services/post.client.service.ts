9a0842df1acddf2312a752ed8b91c3ca8ab5e9be
Amir-Fayyaz <fayamir54@gmail.com>
2025-02-25 04:43:49 -0800

    add getPosts service to post.client.service.ts

---
+++ src/module/post/client/services/post.client.service.ts
@@ -0,0 +1,43 @@
+import { Injectable, UseGuards } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { PostEntity } from '../../entities/post.entity';
+import { Repository } from 'typeorm';
+import { PaginationTool } from 'src/common/utils/pagination.util';
+import { PostSorting } from '../../enums/Post.sorting.enum';
+import { UserGuard } from 'src/module/auth/guards/user.guard';
+
+@UseGuards(UserGuard)
+@Injectable()
+export class PostClientService {
+  constructor(
+    @InjectRepository(PostEntity)
+    private readonly Post_Repository: Repository<PostEntity>,
+  ) {}
+
+  // private services
+
+  // public services
+  public async getPosts(page: number, Sorting: PostSorting) {
+    const pagination = PaginationTool({ page, take: 20 });
+
+    const queryBuilder = this.Post_Repository.createQueryBuilder('post');
+
+    if (Sorting === PostSorting.NEW)
+      queryBuilder.orderBy('post.createdAt', 'DESC');
+    if (Sorting === PostSorting.POPULAR)
+      queryBuilder.orderBy('post.views', 'DESC');
+
+    const [posts, totalCount] = await queryBuilder
+      .leftJoinAndSelect('post.author', 'author')
+      .leftJoinAndSelect('post.subcategory', 'subcategory')
+      .leftJoinAndSelect('subcategory.category', 'category')
+      .skip(pagination.skip)
+      .take(pagination.take)
+      .getManyAndCount();
+
+    return {
+      totalPages: Math.ceil(totalCount / pagination.take),
+      posts,
+    };
+  }
+}
