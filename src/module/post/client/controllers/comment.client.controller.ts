e54917d1193803ff305cf7c02121547456952704
Amir-Fayyaz <fayamir54@gmail.com>
2025-02-26 11:19:09 -0800

    add create-comment api to app

--- src/module/post/client/controllers/comment.client.controller.ts
+++ src/module/post/client/controllers/comment.client.controller.ts
@@ -1,11 +1,40 @@
-import { Controller, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { CommentClientService } from '../services/comment.client.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { UserGuard } from 'src/module/auth/guards/user.guard';
+import { CreateCommentDto } from '../dto/create-Comment.dto';
+import { User } from 'src/common/decorators/getUser.decorator';
+import { UserEntity } from 'src/module/users/entities/user.entity';
 
 @ApiTags('client-comment')
 @UseGuards(UserGuard)
 @Controller('api/v1/client/comment')
 export class CommentClientController {
   constructor(private readonly CommentService: CommentClientService) {}
+
+  @Post(':id')
+  @ApiOperation({
+    summary: 'for add a newComment',
+  })
+  @HttpCode(HttpStatus.CREATED)
+  async createComment(
+    @Param('id', ParseIntPipe) postId: number,
+    @Body() createCommentDto: CreateCommentDto,
+    @User() user: UserEntity,
+  ) {
+    return await this.CommentService.createComment(
+      createCommentDto,
+      user,
+      postId,
+    );
+  }
 }
