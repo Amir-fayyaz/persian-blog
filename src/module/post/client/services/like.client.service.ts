a254cb38bb6bdae28f80f1195484cb22a6fc800b
Amir-Fayyaz <fayamir54@gmail.com>
2025-02-25 16:04:20 -0800

    add likeOrDisLike-post service to like.client.service.ts

--- src/module/post/client/services/like.client.service.ts
+++ src/module/post/client/services/like.client.service.ts
@@ -2,15 +2,27 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { LikeEntity } from '../../entities/like.entity';
 import { Repository } from 'typeorm';
+import { UserEntity } from 'src/module/users/entities/user.entity';
+import { PostClientFactory } from '../post.client.factory';
 
 @Injectable()
 export class LikeClientService {
   constructor(
     @InjectRepository(LikeEntity)
     private readonly Like_Repository: Repository<LikeEntity>,
+    private readonly PostClientFactory: PostClientFactory,
   ) {}
 
   // private methods
+
+  private async isPostLiked(postId: number, userId: number) {
+    return await this.Like_Repository.exists({
+      where: {
+        user: { id: userId },
+        post: { id: postId },
+      },
+    });
+  }
 
   //public methods
   public async getLikesForPost(postId: number) {
@@ -36,5 +48,32 @@ export class LikeClientService {
       count,
       likes,
     };
+  }
+
+  public async LikeAndDislikePost(postId: number, user: UserEntity) {
+    const post = await this.PostClientFactory.FindPostById(postId);
+
+    if (!post) throw new NotFoundException('There is no post with this id');
+
+    const isLiked = await this.isPostLiked(postId, user.id);
+
+    if (isLiked) {
+      await this.Like_Repository.delete({
+        post: { id: postId },
+        user: { id: user.id },
+      });
+
+      return {
+        status: 'Post disliked',
+      };
+    } else {
+      const newLike = await this.Like_Repository.create({ user, post });
+
+      await this.Like_Repository.save(newLike);
+
+      return {
+        status: 'Post liked',
+      };
+    }
   }
 }
