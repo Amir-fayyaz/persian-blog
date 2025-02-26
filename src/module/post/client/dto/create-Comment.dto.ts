982e62560226fd35bf828a3bf01611ff908fda0a
Amir-Fayyaz <fayamir54@gmail.com>
2025-02-26 09:32:00 -0800

    fix them for git

--- src/module/post/client/dto/create-Comment.dto.ts
+++ src/module/post/client/dto/create-Comment.dto.ts
@@ -1,11 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
-<<<<<<< HEAD
 export class CreateCommentDto {
-=======
-export class CreatecommentDto {
->>>>>>> 490f1bc8f74ca77608be15cfd3d8078d0cbfebb1
   @IsString()
   @IsNotEmpty()
   @ApiProperty()
