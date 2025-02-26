48d6a7b803b6c956935f895b7068b1a55295d2e0
Amir-Fayyaz <fayamir54@gmail.com>
2025-02-26 13:51:57 -0800

    add create report-post service in post-report.client.ts

--- src/module/post/client/services/post-report.client.service.ts
+++ src/module/post/client/services/post-report.client.service.ts
@@ -1,6 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { PostReportEntity } from '../../entities/postReport.entity';
+import { Repository } from 'typeorm';
+import { PostClientFactory } from '../post.client.factory';
+import { UserEntity } from 'src/module/users/entities/user.entity';
 
 @Injectable()
 export class PostReportClientService {
-  constructor() {}
+  constructor(
+    @InjectRepository(PostReportEntity)
+    private readonly postReport_Repository: Repository<PostReportEntity>,
+    private readonly PostFactory: PostClientFactory,
+  ) {}
+
+  //private methods
+
+  // public methods
+  public async createPostReport(postId: number, user: UserEntity) {
+    const post = await this.PostFactory.FindPostById(postId);
+
+    if (!post) throw new NotFoundException('There is no post to report');
+
+    const newReport = this.postReport_Repository.create({ post, user });
+
+    return await this.postReport_Repository.save(newReport);
+  }
 }
