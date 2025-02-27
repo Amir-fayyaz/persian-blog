fc285391c9cd7f208375427e969bb3d18d0923b3
Amir-Fayyaz <fayamir54@gmail.com>
2025-02-27 02:47:20 -0800

    add delete-PostReport service to post-report.client.service.ts

--- src/module/post/client/services/post-report.client.service.ts
+++ src/module/post/client/services/post-report.client.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { PostReportEntity } from '../../entities/postReport.entity';
 import { Repository } from 'typeorm';
@@ -21,8 +25,39 @@ export class PostReportClientService {
 
     if (!post) throw new NotFoundException('There is no post to report');
 
+    const isReported = await this.postReport_Repository.findOne({
+      where: {
+        post: { id: postId },
+        user: { id: user.id },
+      },
+    });
+
+    if (isReported) {
+      throw new ConflictException('You reported this post');
+    }
+
     const newReport = this.postReport_Repository.create({ post, user });
 
     return await this.postReport_Repository.save(newReport);
+  }
+
+  public async deletePostReport(postId: number, user: UserEntity) {
+    const Report = await this.postReport_Repository.findOne({
+      where: {
+        post: {
+          id: postId,
+        },
+        user: {
+          id: user.id,
+        },
+      },
+    });
+
+    if (!Report)
+      throw new NotFoundException('There is no report for this post');
+
+    await this.postReport_Repository.remove(Report);
+
+    return { success: true };
   }
 }
