3eef177068c6052ddf65a50d0ccd615172fdcbb5
Amir-Fayyaz <fayamir54@gmail.com>
2025-02-28 09:52:07 -0800

    add getNotificationById api to app

--- src/module/users/client/controllers/notification.client.controller.ts
+++ src/module/users/client/controllers/notification.client.controller.ts
@@ -3,12 +3,19 @@ import {
   Get,
   HttpCode,
   HttpStatus,
+  Param,
+  ParseIntPipe,
   UseGuards,
 } from '@nestjs/common';
 import { NotificationClientService } from '../services/notification.client.service';
 import { User } from 'src/common/decorators/getUser.decorator';
 import { UserEntity } from '../../entities/user.entity';
-import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiParam,
+  ApiTags,
+} from '@nestjs/swagger';
 import { UserGuard } from 'src/module/auth/guards/user.guard';
 
 @ApiTags('client-notifications')
@@ -20,6 +27,7 @@ export class NotificationClientController {
     private readonly NotificationService: NotificationClientService,
   ) {}
 
+  //GET -
   @Get()
   @ApiOperation({
     summary: 'For recive all user notifications',
@@ -27,5 +35,14 @@ export class NotificationClientController {
   @HttpCode(HttpStatus.OK)
   async getUserNotifications(@User() user: UserEntity) {
     return await this.NotificationService.getUserNotifications(user.id);
+  }
+
+  //GET -
+  @Get(':id')
+  @ApiOperation({ summary: 'For recive sepcial notification by id' })
+  @ApiParam({ name: 'id', description: 'notification-id' })
+  @HttpCode(HttpStatus.OK)
+  async getNotificationById(@Param('id', ParseIntPipe) id: number) {
+    return await this.NotificationService.getNotificationById(id);
   }
 }
