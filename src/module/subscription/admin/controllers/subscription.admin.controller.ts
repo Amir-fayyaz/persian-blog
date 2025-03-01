3d6f80339dded1ba924e8da8a4f4d132617d2c21
Amir-Fayyaz <fayamir54@gmail.com>
2025-03-01 13:45:11 -0800

    add create-subscription api to app

--- src/module/subscription/admin/controllers/subscription.admin.controller.ts
+++ src/module/subscription/admin/controllers/subscription.admin.controller.ts
@@ -1,19 +1,23 @@
 import {
+  Body,
   Controller,
   DefaultValuePipe,
   Get,
   HttpCode,
   HttpStatus,
   ParseIntPipe,
+  Post,
   Query,
 } from '@nestjs/common';
 import { SubscriptionAdminService } from '../services/subscription.admin.service';
-import { ApiOperation, ApiQuery } from '@nestjs/swagger';
+import { ApiBody, ApiOperation, ApiQuery } from '@nestjs/swagger';
+import { CreateSubscriptionDto } from '../dto/create-subscription.dto';
 
 @Controller('api/v1/admin/subscriptions')
 export class SubscriptionAdminController {
   constructor(private readonly SubscriptionService: SubscriptionAdminService) {}
 
+  //GET -
   @Get()
   @ApiOperation({ summary: 'For get every subscription with pagination' })
   @ApiQuery({ name: 'page', type: Number, description: 'For pagination' })
@@ -22,5 +26,14 @@ export class SubscriptionAdminController {
     @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
   ) {
     return await this.SubscriptionService.getAllSubscriptions(page);
+  }
+
+  //POST -
+  @Post()
+  @ApiOperation({ summary: 'For create new subscription' })
+  @ApiBody({ type: CreateSubscriptionDto, description: 'required fields' })
+  @HttpCode(HttpStatus.CREATED)
+  async createSubscription(@Body() data: CreateSubscriptionDto) {
+    return await this.SubscriptionService.createSubscription(data);
   }
 }
