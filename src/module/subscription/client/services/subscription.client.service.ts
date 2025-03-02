7cc4d0fb38209076b5f9f071405cdfeffe0f378c
Amir-Fayyaz <fayamir54@gmail.com>
2025-03-01 16:14:19 -0800

    add getSubscription service to subscription.client.service.ts

--- src/module/subscription/client/services/subscription.client.service.ts
+++ src/module/subscription/client/services/subscription.client.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { SubscriptionEntity } from '../../entities/subscription.entity';
 import { Repository } from 'typeorm';
+import { PaginationTool } from 'src/common/utils/pagination.util';
 
 @Injectable()
 export class SubscriptionClientService {
@@ -9,4 +10,23 @@ export class SubscriptionClientService {
     @InjectRepository(SubscriptionEntity)
     private readonly Subscription_Repository: Repository<SubscriptionEntity>,
   ) {}
+
+  //private methods
+
+  //public methods
+  public async getSubscriptions(page: number) {
+    const pagination = PaginationTool({ page, take: 20 });
+
+    const subscriptions = await this.Subscription_Repository.find({
+      order: { durationDays: 'ASC' },
+      take: pagination.take,
+      skip: pagination.skip,
+    });
+
+    return {
+      page,
+      subscriptions,
+      count: subscriptions.length,
+    };
+  }
 }
