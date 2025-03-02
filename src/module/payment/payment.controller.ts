93140a2a7fd5f763bffe791afb735d192d185696
Amir-Fayyaz <fayamir54@gmail.com>
2025-03-01 16:16:30 -0800

    add payment requiremnts

--- src/module/payment/payment.controller.ts
+++ src/module/payment/payment.controller.ts
@@ -1,6 +1,7 @@
 import { Controller } from '@nestjs/common';
+import { PaymentService } from './payment.service';
 
 @Controller('api/v1/payment')
 export class PaymentController {
-  constructor() {}
+  constructor(private readonly PaymentService: PaymentService) {}
 }
--- src/module/payment/payment.service.ts
+++ src/module/payment/payment.service.ts
@@ -1,6 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { PaymentEntity } from './entities/payment.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class PaymentService {
-  constructor() {}
+  constructor(
+    @InjectRepository(PaymentEntity)
+    private readonly Payment_Repository: Repository<PaymentEntity>,
+  ) {}
 }
