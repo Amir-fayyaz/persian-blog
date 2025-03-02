3f51513c90cb2dcd64aebabcd184a146510a3f29
Amir-Fayyaz <fayamir54@gmail.com>
2025-03-01 16:14:55 -0800

    add getSubscription api to app

--- src/module/subscription/client/controllers/subscription.client.controller.ts
+++ src/module/subscription/client/controllers/subscription.client.controller.ts
@@ -1,10 +1,32 @@
-import { Controller, UseGuards } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  Controller,
+  DefaultValuePipe,
+  Get,
+  HttpCode,
+  HttpStatus,
+  ParseIntPipe,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
+import { ApiOperation, ApiQuery, ApiTags } from '@nestjs/swagger';
 import { UserGuard } from 'src/module/auth/guards/user.guard';
+import { SubscriptionClientService } from '../services/subscription.client.service';
 
 @ApiTags('client-subscription')
 @UseGuards(UserGuard)
 @Controller('api/v1/client/subscriptions')
 export class SubscriptionClientController {
-  constructor() {}
+  constructor(
+    private readonly SubscriptionService: SubscriptionClientService,
+  ) {}
+
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  @ApiOperation({ summary: 'For get all subsctription' })
+  @ApiQuery({ name: 'page', description: 'For pagination', type: Number })
+  async getSubscriptions(
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+  ) {
+    return await this.SubscriptionService.getSubscriptions(page);
+  }
 }
